75c8f9d5cdab2938c63954cdd6030037866308a4
Naman Pandey <namanpandey836@gmail.com>
2024-01-19 21:47:21 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -6,26 +6,20 @@ const path = require("path");
 mongoDB();
 const cors = require("cors");
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://foodexpress-nine.vercel.app");
+  next();
+});
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","https://foodexpress-nine.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin","https://foodexpress-nine.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 }) 
-var filePath = "../build/index.html"
-var resolvedPath = path.resolve(filePath);
 
-
-// app.use(express.static(path.join(__dirname, "../build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname,resolvedPath), function (err) {
-//     if (err) {
-//       res.status(500).send(err);
-//     }
-//   });
-// });
 app.get('/', (req, res) => {
   res.status(200).json('Hello World!---------')
 })
