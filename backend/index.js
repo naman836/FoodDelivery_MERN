995db645fe53bc03f974338f1d42c6662553b2e8
Naman Pandey <namanpandey836@gmail.com>
2024-01-19 19:50:41 +0530

    nedw

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use((req,res,next)=>{
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!---------')
+  res.status(200).json('Hello World!---------')
 })
 app.use(express.json())
 app.use('/api', require("./Routes/CreateUser"));
