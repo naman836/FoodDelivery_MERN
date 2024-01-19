46dc2f97bee8b9b4e120a7a596aee9446365589c
Naman Pandey <namanpandey836@gmail.com>
2024-01-19 21:31:33 +0530

    newsgh

--- backend/index.js
+++ backend/index.js
@@ -18,17 +18,17 @@ var filePath = "../build/index.html"
 var resolvedPath = path.resolve(filePath);
 
 
-app.use(express.static(path.join(__dirname, "../build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname,resolvedPath), function (err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  });
-});
-// app.get('/', (req, res) => {
-//   res.status(200).json('Hello World!---------')
-// })
+// app.use(express.static(path.join(__dirname, "../build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname,resolvedPath), function (err) {
+//     if (err) {
+//       res.status(500).send(err);
+//     }
+//   });
+// });
+app.get('/', (req, res) => {
+  res.status(200).json('Hello World!---------')
+})
 app.use(express.json())
 app.use('/api', require("./Routes/CreateUser"));
 app.use('/api', require("./Routes/DisplayData")); 
