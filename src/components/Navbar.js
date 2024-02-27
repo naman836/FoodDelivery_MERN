285d4a5237aed4a47e4952a58102464679a75d7f
Naman Pandey <namanpandey836@gmail.com>
2024-02-27 22:19:23 +0530

    my Commmit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
             <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky"
              style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand fs-1 fst-italic text-warning fw-bold" to="/">FoodExpress</Link>
+                    <Link className="navbar-brand fs-1 fst-italic text-secondary fw-bold" to="/">FoodExpress</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
