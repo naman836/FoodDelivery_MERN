836dae4c808fa1682085c281611a6ee3b11967c0
Naman Pandey <namanpandey836@gmail.com>
2024-02-27 22:06:30 +0530

    my Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,20 +16,21 @@ export default function Navbar() {
     return ( 
         <div>
 
-            <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-success position-sticky"
+             style={{ boxShadow: "0px 10px 20px black", filter: 'blur(20)', position: "fixed", zIndex: "10", width: "100%" }}>
                 <div className="container-fluid">
                     <Link className="navbar-brand fs-1 fst-italic " to="/">FoodExpress</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav me-auto mb-2">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
+                                <Link className="nav-link active fs-5 mx-3 active" aria-current="page" to="/">Home</Link>
                             </li>
                             {(localStorage.getItem("authToken"))?
                               <li className="nav-item">
-                              <Link className="nav-link active fs-5" aria-current="page" to="/myOrder">My Orders</Link>
+                              <Link className="nav-link active fs-5 mx-3 active" aria-current="page" to="/myOrder">My Orders</Link>
                              </li>
                             : ""} 
                         </ul>
